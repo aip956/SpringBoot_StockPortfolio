9351d53d512145d38963a32cfeafc47c550669d7
Anthea Ip <antheaip964@gmail.com>
2023-10-19 21:30:46 -0400

    Saving: stock, edit, delete, add work.

--- src/main/java/com/example/stockportfolio/controller/stockController.java
+++ src/main/java/com/example/stockportfolio/controller/stockController.java
@@ -4,8 +4,7 @@ import com.example.stockportfolio.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,16 +26,50 @@ public class stockController {
 
     @GetMapping("/stock")
     public String showStock(Model model) {
-        List<Stock> stock = stockService.getAllStock();
-        model.addAttribute("stock", stock);
+        List<Stock> stocks = stockService.getAllStock();
+        System.out.println("In showStock" + stocks);
+        model.addAttribute("stocks", stocks);
         return "showStocks";
     }
 
     @GetMapping("/addStock")
     public String showAddStock(Model model) {
         model.addAttribute("stock", new Stock());
+
         // return addStock.html form
         return "addStock";
     }
 
+    @PostMapping("/addStock")
+    public String addStock(@ModelAttribute Stock stock) {
+        Stock savedStock = stockService.saveStock(stock);
+        System.out.println("In addStock: " + stock);
+        // saves the stock after adding
+        return "redirect:/stock";
+    }
+
+    @GetMapping("/editStock/{id}")
+    public String editStock(@PathVariable Long id, Model model) {
+        Stock stockToEdit = stockService.findStockById(id);
+        if(stockToEdit != null) {
+            model.addAttribute("stock", stockToEdit);
+            return "editStock";
+
+        } else {
+            return "stockNotFound";
+        }
+    }
+
+    @PostMapping("/updateStock")
+    public String updateStock(@ModelAttribute Stock stock) {
+        stockService.saveStock(stock);
+        // saves the stock after update and redirects to stock page
+        return "redirect:/stock";
+    }
+
+    @GetMapping("/deleteStock/{id}")
+    public String deleteStock(@PathVariable Long id, Model model) {
+        stockService.deleteStockById(id);
+        return "redirect:/stock";
+    }
 }
