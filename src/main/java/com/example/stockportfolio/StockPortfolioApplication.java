505a33b07c68b3006d80079e5f459e45d1d6421a
Anthea Ip <antheaip964@gmail.com>
2023-11-28 13:37:04 -0500

    Updated main class for default port

--- src/main/java/com/example/stockportfolio/StockPortfolioApplication.java
+++ src/main/java/com/example/stockportfolio/StockPortfolioApplication.java
@@ -3,12 +3,22 @@ package com.example.stockportfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class StockPortfolioApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockPortfolioApplication.class, args);
+        // Use PORT provided in environment or default to 8080
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            // default to 8080 if PORT not set
+            port = "8080";
+        }
+        // Set the port and start the application
+        SpringApplication app = new SpringApplication(StockPortfolioApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        app.run(args);
         System.out.println("Now running!");
     }
-
 }
